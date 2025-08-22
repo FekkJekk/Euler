96b9e8252d726c9bf8a1e575ed954ed2d723be68
FekkJekk <227577743+FekkJekk@users.noreply.github.com>
2025-08-22 20:35:19 +0100

    Update RyanMain.py

--- task2/RyanMain.py
+++ task2/RyanMain.py
@@ -1,11 +1,11 @@
-z = 0
-x = 2
-y = 1
-a = 0
-while x < 4000000:
-    if x % 2 == 0:
-        a += x
-    z = x + y
-    y = x
-    x = z
-print(a)
+NextNum = 0
+CurrentNum = 2
+PrevNum = 1
+Sum = 0 #sum of even numbers found
+while CurrentNum < 4000000:
+    if CurrentNum % 2 == 0:
+        Sum += CurrentNum
+    NextNum = CurrentNum + PrevNum
+    PrevNum = CurrentNum
+    CurrentNum = NextNum
+print(Sum)
