064a1874e61b380e085cfd1eeb20d3ea9250d836
FekkJekk <227577743+FekkJekk@users.noreply.github.com>
2025-08-24 19:51:42 +0100

    Update RyanMain.py

--- task3/RyanMain.py
+++ task3/RyanMain.py
@@ -1,18 +1,22 @@
-test = 10
+test = 11
 for lowest in range(2,test):
     if test % lowest == 0:
         break
 print(lowest)
 print('test / lowest factor:',test/lowest)
 for possible_highest in range(int(test/lowest),1,-1):
+    prime = 1
     for test in range(possible_highest,1,-1):
         print(test)
-        prime = 0
         if possible_highest != test and possible_highest % test == 0:
             prime = 0
             print('tested')
             print('highest prime is',possible_highest)
             break
-        if prime == 0:
-            print('test2')
-            break
+        #if prime == 0:
+        #    print('test2')
+        #    break
+    print('oktest')
+    if prime == 1:
+        print(possible_highest,'is the highest prime factor')
+        break
